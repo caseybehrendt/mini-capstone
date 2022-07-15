339bdd5a0f35e616160f4b9ea8717a6e3fc6ae67
CASEY BEHRENDT <behrendtc@gmail.com>
2022-07-15 11:33:55 -0600

    adds in has many through associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,19 @@
 class Product < ApplicationRecord
   belongs_to :supplier
+  has_many :order
+  has_many :category_products
+  has_many :categories, through :category_products
+  
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..600 }
+
+
+  def categories
+    category_products.map do |category_product|
+      category_product.category
+    end
+  end
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
